1f5df0931970ed800c95657ea76d2e4388f09cbc
Artyom Romaskevich <artisgs3@gmail.com>
2022-05-01 22:41:33 +0300

    feat: vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,85 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.isDirect = direct;
+    this.alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.square = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                   'BCDEFGHIJKLMNOPQRSTUVWXYZA',
+                   'CDEFGHIJKLMNOPQRSTUVWXYZAB',
+                   'DEFGHIJKLMNOPQRSTUVWXYZABC',
+                   'EFGHIJKLMNOPQRSTUVWXYZABCD',
+                   'FGHIJKLMNOPQRSTUVWXYZABCDE',
+                   'GHIJKLMNOPQRSTUVWXYZABCDEF',
+                   'HIJKLMNOPQRSTUVWXYZABCDEFG',
+                   'IJKLMNOPQRSTUVWXYZABCDEFGH',
+                   'JKLMNOPQRSTUVWXYZABCDEFGHI',
+                   'KLMNOPQRSTUVWXYZABCDEFGHIJ',
+                   'LMNOPQRSTUVWXYZABCDEFGHIJK',
+                   'MNOPQRSTUVWXYZABCDEFGHIJKL',
+                   'NOPQRSTUVWXYZABCDEFGHIJKLM',
+                   'OPQRSTUVWXYZABCDEFGHIJKLMN',
+                   'PQRSTUVWXYZABCDEFGHIJKLMNO',
+                   'QRSTUVWXYZABCDEFGHIJKLMNOP',
+                   'RSTUVWXYZABCDEFGHIJKLMNOPQ',
+                   'STUVWXYZABCDEFGHIJKLMNOPQR',
+                   'TUVWXYZABCDEFGHIJKLMNOPQRS',
+                   'UVWXYZABCDEFGHIJKLMNOPQRST',
+                   'VWXYZABCDEFGHIJKLMNOPQRSTU',
+                   'WXYZABCDEFGHIJKLMNOPQRSTUV',
+                   'XYZABCDEFGHIJKLMNOPQRSTUVW',
+                   'YZABCDEFGHIJKLMNOPQRSTUVWX',
+                   'ZABCDEFGHIJKLMNOPQRSTUVWXY'];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const messageUP = message.toUpperCase();
+    const keyUP = key.toUpperCase();
+    let res = '';
+    for (let m = 0, k = 0; m < messageUP.length; m++, k++) {
+      if (this.alphabet.indexOf(messageUP[m]) < 0) {
+        res += messageUP[m];
+        k--;
+      } else {
+        let i = this.alphabet.indexOf(messageUP[m]);
+        let j = this.alphabet.indexOf(keyUP[k % keyUP.length]);
+        res += this.square[i][j];
+      }
+    }
+    if (this.isDirect) {
+      return res;
+    } else {
+      return res.split("").reverse().join("");
+    }
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const messageUP = message.toUpperCase();
+    const keyUP = key.toUpperCase();
+    let res = '';
+    for (let m = 0, k = 0; m < messageUP.length; m++, k++) {
+      if (this.alphabet.indexOf(messageUP[m]) < 0) {
+        res += messageUP[m];
+        k--;
+      } else {
+        let i = this.alphabet.indexOf(keyUP[k % keyUP.length]);
+        let j = this.square[i].indexOf(messageUP[m]);
+        res += this.alphabet[j];
+      }
+    }
+    if (this.isDirect) {
+      return res;
+    } else {
+      return res.split("").reverse().join("");
+    }
   }
 }
 
